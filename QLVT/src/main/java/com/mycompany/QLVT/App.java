bd28bbf832c2ab8793c09d9a634358ca7135696a
zoroONE01 <lekimnguyen671@gmail.com>
2021-09-05 14:59:00 +0700

    don sach se con hang

--- QLVT/src/main/java/com/mycompany/QLVT/App.java
+++ QLVT/src/main/java/com/mycompany/QLVT/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
         stage.show();
     }
@@ -27,24 +27,19 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-       System.out.println(App.class.getResource("../../../fxml/FXML" + ".fxml"));
-       System.out.println(App.class.getResource("./DemoController/demoFXML" + ".fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("../../../fxml/" + fxml + ".fxml"));
         return fxmlLoader.load();//return Root node(Parent node)
     }
 
     public static void main(String[] args) {
         launch();
     }
-    
+
     /*FXMLLoader.load()
         create java object tha represent View
         create Controller ( fire Contructor)
         connect View nodes with Controller fileds
         Fire controller initialize() method
         return root node
-    */
-   
-    
-
+     */
 }
