bc0723cfff8018fe8fa22333d12bf610b29836ab
zoroONE01 <lekimnguyen671@gmail.com>
2021-09-05 14:36:47 +0700

    test git

--- QLVT/src/main/java/com/mycompany/QLVT/controller/NhanVienViewController.java
+++ QLVT/src/main/java/com/mycompany/QLVT/controller/NhanVienViewController.java
@@ -27,7 +27,7 @@ public class NhanVienViewController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        // hello
     }    
     
 }
